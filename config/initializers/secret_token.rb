47381b7c9616f1911e19743ec6732a7240b7fe20
cookr <ciaran@cookr.com>
2013-01-06 10:17:21 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookrdev::Application.config.secret_token = '546a207daaf295bf921d722f91d52d1edfc753ab71b87f71b8fd202b2124946decaf0f42c8cdca2207be1698c49ba5ed3fc23675a3b9620a2ad9bcfafb9a1bb2'
